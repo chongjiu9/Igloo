a382d246416d1a8efbc0c08801195f381d286c79
chongjiu <justinagao9@gmail.com>
2022-01-22 11:16:34 -0800

    update main page and add left and right panel

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Intro from './componenets/intro/intro';
+
+function App() {
+  return (
+    <div>
+      <Intro/>
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/componenets/intro/intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./intro.css"
+
+const Intro = () => {
+  return <div className='i'>
+      {/* split up the screen to left and right */}
+      <div className="i-left">left</div>
+      <div className="i-right">right</div>
+
+    </div>;
+}
+
+export default Intro
+

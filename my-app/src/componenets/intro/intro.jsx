7b3660f415e4c3132ca7601e9c376b9a4eff7d14
chongjiu <justinagao9@gmail.com>
2022-01-22 11:20:19 -0800

    add intro

--- my-app/src/componenets/intro/intro.jsx
+++ my-app/src/componenets/intro/intro.jsx
@@ -4,7 +4,15 @@ import "./intro.css"
 const Intro = () => {
   return <div className='i'>
       {/* split up the screen to left and right */}
-      <div className="i-left">left</div>
+      <div className="i-left">
+
+           {/* wrapper of website intro name */}
+          <div className="i-left-wrapper">
+              <h2 className='i-intro'>Hello, My name is</h2>
+              <h1 className='i-name'>Igloo</h1>
+          </div>
+          
+      </div>
       <div className="i-right">right</div>
 
     </div>;

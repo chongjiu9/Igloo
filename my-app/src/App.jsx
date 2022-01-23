bd4dd27e43cb73f244b9449533a4dae8c4babc80
chongjiu <justinagao9@gmail.com>
2022-01-22 16:49:40 -0800

    create about

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Intro from './componenets/intro/intro';
+import About from './componenets/about/About';
+;
+
 
 function App() {
   return (
     <div>
       <Intro/>
+      <About/>
  
     </div>
   );
---
+++ my-app/src/componenets/about/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => {
+  return <div className='a'>
+      about
+
+  </div>;
+};
+
+export default About
